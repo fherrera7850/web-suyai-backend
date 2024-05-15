fee3a046096390023296e8043d3436e05c23e2e5
Felipe Herrera <fherrera7850@gmail.com>
2024-05-15 18:36:50 -0400

    conectado a base local funcionando

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 (async () => {
-  const db = await mongoose.connect("mongodb://localhost/simplejwt", {
+  const db = await mongoose.connect("mongodb://localhost:27017/web-suyai", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from "./app";
 import "./database";
 
-app.listen(3000);
-console.log("Server on port 3000");
+app.listen(5000);
+console.log("Server on port 5000");
